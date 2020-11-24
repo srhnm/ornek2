eadcf912a0ab7842be54db0d025f9d952f65447b
sareh <srhnmlgnr34@hotmail.com>
2020-11-24 20:47:13 +0100

    hazir gibi

--- src/test1.java
+++ src/test1.java
@@ -1,6 +1,12 @@
 public class test1 {
     public static void main(String[] args) {
-        System.out.println("1");
-        System.out.println(".gitignore dosyasina hangi dosya veya klasorler gitmeyecekse onlar yazilir");
+        System.out.println("1. islem");
+    }
+    public static void yeniMethod(){
+        System.out.println("yeni methodum hazir");
+        System.out.println("sepete ekle burada");
+    }
+    public static void sepettenSil(){
+        System.out.println("sepeti sil hazir");
     }
 }
